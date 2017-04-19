bf4dc2d8cda5528a57a3162e1246a44b84cadad5
Wang Pengwei <im_today@163.com>
2017-04-19 17:35:37 +0800

    test

--- src/main/java/com/vvsvip/shop/core/action/TestAction.java
+++ src/main/java/com/vvsvip/shop/core/action/TestAction.java
@@ -1,8 +1,18 @@
 package com.vvsvip.shop.core.action;
 
+import com.alibaba.dubbo.common.json.JSONArray;
+import com.alibaba.dubbo.common.json.JSONObject;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by blues on 2017/4/19.
@@ -10,11 +20,34 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TestAction {
 
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @RequestMapping("/toFreemarker")
     public ModelAndView toFreemarker() {
-        ModelAndView modelAndView = new ModelAndView("test.ftl");
+        ModelAndView modelAndView = new ModelAndView("test");
         modelAndView.addObject("title", "Spring MVC And Freemarker");
         modelAndView.addObject("content", " Hello world ， test my first spring mvc ! ");
         return modelAndView;
+    }
+
+    @RequestMapping("/1")
+    public String index1() {
+        return "html/index1";
+    }
+
+    @RequestMapping("/2")
+    public String index2() {
+        return "index1";
+    }
+
+    @RequestMapping("/toJSON")
+    @ResponseBody
+    public Object toJson() {
+        Map<String, String> map = new HashMap<>();
+        map.put("test", "test");
+        return map;
     }
 }
